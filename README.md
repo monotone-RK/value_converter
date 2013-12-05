7e26f7b12a5f9816877620c0b63ecfe63f2aab7f
monotone-RK <kobaya4121@gmail.com>
2013-12-06 04:43:24 +0900

    added format(float,double)

--- README.md
+++ README.md
@@ -8,18 +8,22 @@ Last updated 2013.12.05
     python valconv.py [options] value
     
     opthions:
-    -h, --help     show this help message and exit
-    -v, --version  Show the version and exit
-        --dec2bin  conversion format: dec -> bin
-        --dec2oct  conversion format: dec -> oct
-        --dec2hex  conversion format: dec -> hex
-        --bin2oct  conversion format: bin -> oct
-        --bin2dec  conversion format: bin -> dec
-        --bin2hex  conversion format: bin -> hex
-        --oct2bin  conversion format: oct -> bin
-        --oct2dec  conversion format: oct -> dec
-        --oct2hex  conversion format: oct -> hex
-        --hex2bin  conversion format: hex -> bin
-        --hex2oct  conversion format: hex -> oct
-        --hex2dec  conversion format: hex -> dec
+    -h, --help      show this help message and exit
+    -v, --version   Show the version and exit
+        --dec2bin   conversion format: dec -> bin
+        --dec2oct   conversion format: dec -> oct
+        --dec2hex   conversion format: dec -> hex
+        --bin2oct   conversion format: bin -> oct
+        --bin2dec   conversion format: bin -> dec
+        --bin2hex   conversion format: bin -> hex
+        --oct2bin   conversion format: oct -> bin
+        --oct2dec   conversion format: oct -> dec
+        --oct2hex   conversion format: oct -> hex
+        --hex2bin   conversion format: hex -> bin
+        --hex2oct   conversion format: hex -> oct
+        --hex2dec   conversion format: hex -> dec
+        --floathex  conversion format: dec -> hex (float)
+        --doublehex conversion format: dec -> hex (double)
+        --floatdec  conversion format: hex -> dec (float)
+        --doubledec conversion format: hex -> dec (double)
 
--- valconv.py
+++ valconv.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #*****************************************************************************/
-# Value Converter Written in Python Ver:1.0             2013.12.05 monotone-RK/
+# Value Converter Written in Python Ver:1.1             2013.12.06 monotone-RK/
 #*****************************************************************************/
 import sys
+import ctypes
 from optparse import OptionParser
+
+
+class Single(ctypes.Union):
+    _fields_ = [("f", ctypes.c_float),
+                ("i", ctypes.c_ulong)]
+
+
+class Double(ctypes.Union):
+    _fields_ = [("lf", ctypes.c_double),
+                ("i", ctypes.c_ulonglong)]
 
 
 def usage():
     print "## Value Converter Written in Python"
-    print "## Date:2013.12.05"
+    print "## Date:2013.12.06"
     print "## Usage: python valconv.py [options] value\n"
     print """Options:
-  -h, --help     show this help message and exit
-  -v, --version  Show the version
-      --dec2bin  conversion format: dec -> bin
-      --dec2oct  conversion format: dec -> oct
-      --dec2hex  conversion format: dec -> hex
-      --bin2oct  conversion format: bin -> oct
-      --bin2dec  conversion format: bin -> dec
-      --bin2hex  conversion format: bin -> hex
-      --oct2bin  conversion format: oct -> bin
-      --oct2dec  conversion format: oct -> dec
-      --oct2hex  conversion format: oct -> hex
-      --hex2bin  conversion format: hex -> bin
-      --hex2oct  conversion format: hex -> oct
-      --hex2dec  conversion format: hex -> dec"""
+  -h, --help      show this help message and exit
+  -v, --version   Show the version
+      --dec2bin   conversion format: dec -> bin
+      --dec2oct   conversion format: dec -> oct
+      --dec2hex   conversion format: dec -> hex
+      --bin2oct   conversion format: bin -> oct
+      --bin2dec   conversion format: bin -> dec
+      --bin2hex   conversion format: bin -> hex
+      --oct2bin   conversion format: oct -> bin
+      --oct2dec   conversion format: oct -> dec
+      --oct2hex   conversion format: oct -> hex
+      --hex2bin   conversion format: hex -> bin
+      --hex2oct   conversion format: hex -> oct
+      --hex2dec   conversion format: hex -> dec
+      --floathex  conversion format: dec -> hex (float)
+      --doublehex conversion format: dec -> hex (double)
+      --floatdec  conversion format: hex -> dec (float)
+      --doubledec conversion format: hex -> dec (double)"""
 
 
 def version():
-    print "Value Converter Written in Python v1.0  last upated:2013.12.05"
+    print "Value Converter Written in Python v1.1  last upated:2013.12.06"
 
 
 def valueconverter():
@@ -81,6 +96,22 @@ def valueconverter():
         print "conversion format: hex -> dec"
         print "%s -> %s" % (args[0], str(int(args[0], 16)))
         sys.exit()
+    elif options.floathex:
+        print "conversion format: dec -> hex (float)"
+        print "%s -> %s" % (args[0], hex(int(bin(Single(f=float(args[0])).i), 2)))
+        sys.exit()
+    elif options.doublehex:
+        print "conversion format: dec -> hex (double)"
+        print "%s -> %s" % (args[0], hex(int(bin(Double(lf=float(args[0])).i), 2)))
+        sys.exit()
+    elif options.floatdec:
+        print "conversion format: hex -> dec (float)"
+        print "%s -> %s" % (args[0], str(Single(i=int(args[0], 16)).f))
+        sys.exit()
+    elif options.doubledec:
+        print "conversion format: hex -> dec (double)"
+        print "%s -> %s" % (args[0], str(Double(i=int(args[0], 16)).lf))
+        sys.exit()
     else:
         print "Please input an option(conversion format) you want"
         sys.exit()
@@ -112,6 +143,14 @@ optparser.add_option("--hex2oct", action="store_true", dest="hex2oct",
                      default=False, help="conversion format: hex -> oct")
 optparser.add_option("--hex2dec", action="store_true", dest="hex2dec",
                      default=False, help="conversion format: hex -> dec")
+optparser.add_option("--floathex", action="store_true", dest="floathex",
+                     default=False, help="conversion format: dec -> hex (float)")
+optparser.add_option("--doublehex", action="store_true", dest="doublehex",
+                     default=False, help="conversion format: dec -> hex (double)")
+optparser.add_option("--floatdec", action="store_true", dest="floatdec",
+                     default=False, help="conversion format: hex -> dec (float)")
+optparser.add_option("--doubledec", action="store_true", dest="doubledec",
+                     default=False, help="conversion format: hex -> dec(double)")
 (options, args) = optparser.parse_args()
 
 if options.version:
